77c0da85ad80a9a9951f03c91a6cffc24f476a70
robot <molnagmolang13@gmail.com>
2022-05-23 13:59:13 +0900

    이미지 클릭 이벤트 처리

--- app/src/main/java/com/example/mylandmark/infoActivity.java
+++ app/src/main/java/com/example/mylandmark/infoActivity.java
@@ -1,6 +1,8 @@
 package com.example.mylandmark;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,10 +16,11 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.mylandmark.databinding.ActivityInfoBinding;
 
-public class infoActivity extends AppCompatActivity {
+public class infoActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView img_info;
     TextView txt_info;
+    Button btn_info_back;
     int imgno = 0;
     int imgs[] = {R.drawable.info1 , R.drawable.info2 , R.drawable.info3 , R.drawable.info4
             , R.drawable.info5};
@@ -32,6 +35,23 @@ public class infoActivity extends AppCompatActivity {
 
         img_info = findViewById(R.id.img_info);
         txt_info = findViewById(R.id.txt_info);
+        btn_info_back = findViewById(R.id.btn_info_back);
+
+        img_info.setImageResource(imgs[imgno]);
+        txt_info.setText(txts[imgno]);
+
+        img_info.setOnClickListener(this);
+        btn_info_back.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btn_info_back){
+
+        }else if(view.getId() == R.id.img_info){
+            imgno = ++imgno % 5;
+            img_info.setImageResource();]
+            txt_info.setText((txts[imgno]));
+        }
+    }
 }
