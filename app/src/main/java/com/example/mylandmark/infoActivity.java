45b321712d75bf61b6c82a38851bf46f020218d1
robot <molnagmolang13@gmail.com>
2022-05-23 13:33:19 +0900

    텍스트 추가

--- app/src/main/java/com/example/mylandmark/infoActivity.java
+++ app/src/main/java/com/example/mylandmark/infoActivity.java
@@ -21,6 +21,7 @@ public class infoActivity extends AppCompatActivity {
     int imgno = 0;
     int imgs[] = {R.drawable.info1 , R.drawable.info2 , R.drawable.info3 , R.drawable.info4
             , R.drawable.info5};
+    String txts[] = {"1번 설명", "2번 설명", "3번설명", "4번설명", "5번설명"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
